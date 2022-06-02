a55a3ecbf6626b82780a4471be756bf53502c4e8
Jaime Morais <jaime.morais@serpro.gov.br>
2022-06-02 08:12:30 -0300

    Create api project

---
+++ src/api/BreadApi/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/list", () =>
+{
+    return Enumerable.Empty<string>();
+});
+
+app.Run();
