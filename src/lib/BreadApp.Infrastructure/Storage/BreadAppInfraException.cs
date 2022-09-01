72f89ead073ac3a9576907da8878b20b5c553d48
Jaime Morais <jaimemorais@gmail.com>
2022-09-01 09:07:30 -0300

    Implementing ImageAzureBlogStorageService

---
+++ src/lib/BreadApp.Infrastructure/Storage/BreadAppInfraException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BreadApp.Infrastructure.Storage
+{
+    [Serializable]
+    internal class BreadAppInfraException : Exception
+    {
+        public BreadAppInfraException()
+        {
+        }
+
+        public BreadAppInfraException(string message) : base(message)
+        {
+        }
+
+        public BreadAppInfraException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BreadAppInfraException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- src/lib/BreadApp.Infrastructure/Storage/ImageAzureBlobStorageService.cs
+++ src/lib/BreadApp.Infrastructure/Storage/ImageAzureBlobStorageService.cs
@@ -1,6 +1,8 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using BreadApp.Application.Common.Interfaces.Storage;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BreadApp.Infrastructure.Storage
@@ -9,46 +11,34 @@ namespace BreadApp.Infrastructure.Storage
     {
         const string BREADAPP_PICS_CONTAINER_NAME = "breadapp-pics";
 
+        private readonly IConfiguration _config;
+
+        public ImageAzureBlobStorageService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
         public async Task<string> StoreImageAsync(string fileName, byte[] imageData)
         {
+            // az storage account show-connection - string--name < account_name > --resource - group < resource_group >
+            string connectionString = _config["BreadAppAzure:BlobStorage:ConnectionString"];
 
-            // ref : https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/storage/Azure.Storage.Blobs/samples/Sample01b_HelloWorldAsync.cs#L21
+            BlobContainerClient container = new(connectionString, BREADAPP_PICS_CONTAINER_NAME);
 
-            // Get a connection string to our Azure Storage account.  You can
-            // obtain your connection string from the Azure Portal (click
-            // Access Keys under Settings in the Portal Storage account blade)
-            // or using the Azure CLI with:
-            //
-            //     az storage account show-connection-string --name <account_name> --resource-group <resource_group>
-            //
-            // And you can provide the connection string to your application
-            // using an environment variable.
-            string connectionString = ConnectionString;
+            BlobClient blob = container.GetBlobClient(fileName);
 
-            // Get a reference to a container named "sample-container" and then create it
-            BlobContainerClient container = new BlobContainerClient(connectionString, BREADAPP_PICS_CONTAINER_NAME);
-            await container.CreateAsync();
-            try
+            using var data = new MemoryStream(imageData);
+            await blob.UploadAsync(data);
+
+            BlobProperties properties = await blob.GetPropertiesAsync();
+            if (properties.ContentLength != imageData.Length)
             {
-                // Get a reference to a blob
-                BlobClient blob = container.GetBlobClient(fileName);
-
-                // Upload file data
-                await blob.UploadAsync(imageData);
-
-                // Verify we uploaded some content
-                BlobProperties properties = await blob.GetPropertiesAsync();
-                if (properties.ContentLength != imageData.Length)
-                {
-                    throw BreadAppInfraException("Upload failed");
-                }
-
+                throw new BreadAppInfraException($"Upload failed for image '{fileName}'");
             }
-            finally
-            {
-                // Clean up after the test when we're finished
-                await container.DeleteAsync();
-            }
+
+            // TODO
+            return null;
         }
     }
 }
