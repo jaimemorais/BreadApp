d77b47be9304eb0b214ac0482084ccdf0a9f4235
Jaime Morais <jaimemorais@gmail.com>
2022-09-10 11:46:28 -0300

    Create IMailSenderService and SendgridMailService

---
+++ src/lib/BreadApp.Application/Common/Interfaces/Email/IMailSenderService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace BreadApp.Application.Common.Interfaces.Email
+{
+    public interface IEmailSenderService
+    {
+        Task SendMailAsync(string toEmail, string subject, string body);
+    }
+}
---
+++ src/lib/BreadApp.Infrastructure/Email/SendgridMailService.cs
@@ -0,0 +1,14 @@
+﻿using BreadApp.Application.Common.Interfaces.Email;
+using System;
+using System.Threading.Tasks;
+
+namespace BreadApp.Infrastructure.Email
+{
+    public class SendgridMailService : IEmailSenderService
+    {
+        public async Task SendMailAsync(string toEmail, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
