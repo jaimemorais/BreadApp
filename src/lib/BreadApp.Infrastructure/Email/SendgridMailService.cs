6444263a1b3057e678b2d26502f3e318bc6a7e36
Jaime Morais <jaimemorais@gmail.com>
2022-09-12 08:38:14 -0300

    Add SendgridMailService

--- src/lib/BreadApp.Infrastructure/Email/SendgridMailService.cs
+++ src/lib/BreadApp.Infrastructure/Email/SendgridMailService.cs
@@ -1,14 +1,31 @@
 ﻿using BreadApp.Application.Common.Interfaces.Email;
-using System;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
 
 namespace BreadApp.Infrastructure.Email
 {
     public class SendgridMailService : IEmailSenderService
     {
+        private readonly IConfiguration _config;
+
+        public SendgridMailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
         public async Task SendMailAsync(string toEmail, string subject, string body)
         {
-            throw new NotImplementedException();
+            string apiKey = _config["BreadApp:Sendgrid:ApiKey"];
+            var client = new SendGridClient(apiKey);
+
+            var from = new EmailAddress("breadapp@breadapp.com", "Bread App");
+            var to = new EmailAddress(toEmail);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, subject, subject);
+
+            await client.SendEmailAsync(msg);
         }
     }
 }
