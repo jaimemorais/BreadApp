209d92df95d064b0779be922925366e10e1a7cce
Jaime Morais <jaimemorais@gmail.com>
2022-09-13 08:43:43 -0300

    Add CloudEvent schema dto

---
+++ src/lib/BreadApp.Application/Common/Messaging/Dto/BreadAppCloudEventSchemaV1.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
+
+namespace BreadApp.Application.Common.Messaging.Dto
+{
+    public class BreadAppCloudEventSchemaV1<T> where T : class
+    {
+        [JsonProperty("specversion")]
+        public string CloudEventSpecVersion => "1.0";
+
+        [JsonProperty("type")]
+        public string EventType { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("id")]
+        public string EventId => Guid.NewGuid().ToString();
+
+        [JsonProperty("time")]
+        public string Time => DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+
+
+
+        [JsonProperty("data")]
+        public T Data { get; set; }
+
+    }
+}
