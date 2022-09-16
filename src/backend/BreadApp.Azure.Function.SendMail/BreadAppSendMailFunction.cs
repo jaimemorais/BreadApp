a644c4ae09b02b1ad27196417b60efd6681f799c
Jaime Morais <jaimemorais@gmail.com>
2022-09-16 08:07:49 -0300

    Configure send mail azure function

--- src/backend/BreadApp.Azure.Function.SendMail/BreadAppSendMailFunction.cs
+++ src/backend/BreadApp.Azure.Function.SendMail/BreadAppSendMailFunction.cs
@@ -1,24 +1,24 @@
-using Azure.Messaging.EventGrid;
+using Azure.Messaging;
+using BreadApp.Application.Common.Interfaces.Email;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace BreadApp.Azure.Function.SendMail
 {
-
-
     // Default URL for triggering event grid function in the local environment.
     // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 
     public static class BreadAppSendMailFunction
     {
-        [FunctionName("BreadAppSendMailFunction")]
-        public static void Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
-        {
-            // TODO
-            // await _emailSenderService.SendMailAsync(notification.UserEmail, "Welcome to BreadApp!", $"Welcome to BreadApp {notification.UserEmail} !");
 
-            log.LogInformation(eventGridEvent.Data.ToString());
+        [FunctionName("BreadAppSendMailFunction")]
+        public static void Run([EventGridTrigger] CloudEvent e, ILogger log)
+        {
+            // TODO await _emailSenderService.SendMailAsync(notification.UserEmail, "Welcome to BreadApp!", $"Welcome to BreadApp {notification.UserEmail} !");
+
+            log.LogInformation(e.Data.ToString());
         }
     }
 }
---
+++ src/backend/BreadApp.Azure.Function.SendMail/Startup.cs
@@ -0,0 +1,17 @@
+﻿using BreadApp.Application.Common.Interfaces.Email;
+using BreadApp.Infrastructure.Email;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(BreadApp.Azure.Function.SendMail.Startup))]
+
+namespace BreadApp.Azure.Function.SendMail
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped<IEmailSenderService, SendgridMailService>();
+        }
+    }
+}
