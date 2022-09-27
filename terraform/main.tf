73d37bf84f8cd69111f3da9af9e259584a8703fb
Jaime Morais <jaimemorais@gmail.com>
2022-09-27 08:50:43 -0300

    Create terraform eventgrid topic config

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,19 +13,31 @@ terraform {
   backend "azurerm" {}
 }
 
-resource "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
-    location = var.location
-    tags = var.tags
+resource "azurerm_resource_group" "breadpp-tf-rg" {
+    name = var.breadapp_resource_group_name
+    location = var.breadapp_location
+    tags = var.breadapp_tags
 }
 
-resource "azurerm_storage_account" "storage" {
-  name = var.storage_account_name
-  resource_group_name = var.resource_group_name
-  location = var.location
-  tags = var.tags
+resource "azurerm_storage_account" "breadpp-tf-storage" {
+  name = var.breadapp_storage_account_name
+  resource_group_name = var.breadapp_resource_group_name
+  location = var.breadapp_location
+  tags = var.breadapp_tags
   account_tier = "Standard"
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_container" "breadapp-tf-blob-storage-pics" {
+  name                  = var.breadapp_storage_pics_blob_container_name
+  storage_account_name  = var.breadapp_storage_account_name
+  container_access_type = "private"
+}
+
+resource "azurerm_eventgrid_topic" "breadpp-tf-eventgrid-sendmail-topic" {
+  name                = var.breadapp_eventgrid_sendmail_topic
+  location            = var.breadapp_location
+  resource_group_name = var.breadapp_resource_group_name
+  tags                = var.breadapp_tags
+}
 
