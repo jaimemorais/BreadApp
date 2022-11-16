f18854a7b936d333e73814a8b304e8e6de4d7935
Jaime Morais <jaimemorais@gmail.com>
2022-11-16 08:29:02 -0300

    Update function

--- src/backend/BreadApp.Azure.Function.SendMail/BreadAppSendMailFunction.cs
+++ src/backend/BreadApp.Azure.Function.SendMail/BreadAppSendMailFunction.cs
@@ -13,17 +13,20 @@ namespace BreadApp.Azure.Function.SendMail
 
     public class BreadAppSendMailFunction
     {
+        private readonly ILogger _logger;
 
         private readonly IEmailSenderService _emailSenderService;
 
-        public BreadAppSendMailFunction(IEmailSenderService emailSenderService)
+        public BreadAppSendMailFunction(IEmailSenderService emailSenderService, ILoggerFactory loggerFactory)
         {
             _emailSenderService = emailSenderService;
+            _logger = loggerFactory.CreateLogger<BreadAppSendMailFunction>();
+
         }
 
 
         [FunctionName("BreadAppSendMailFunction")]
-        public async Task Run([EventGridTrigger] CloudEvent e, ILogger log)
+        public async Task Run([EventGridTrigger] CloudEvent e)
         {
             NewUserRegisteredDomainEvent newUserRegisteredDomainEvent = e.Data.ToObjectFromJson<NewUserRegisteredDomainEvent>();
 
@@ -31,7 +34,7 @@ namespace BreadApp.Azure.Function.SendMail
             await _emailSenderService.SendMailAsync(newUserRegisteredDomainEvent.UserEmail,
                 "Welcome to BreadApp!", $"Welcome to BreadApp {newUserRegisteredDomainEvent.UserName} !");
 
-            log.LogInformation("Email sent. Data : " + e.Data.ToString());
+            _logger.LogInformation("Email sent. Data : " + e.Data.ToString());
         }
     }
 }
---
+++ src/backend/BreadApp.Azure.Function.SendMail/Program.cs
@@ -0,0 +1,17 @@
+﻿using BreadApp.Application.Common.Interfaces.Email;
+using BreadApp.Infrastructure.Email;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(s =>
+    {
+        s.AddSingleton<IEmailSenderService, SendgridMailService>();
+    })
+            .Build();
+
+host.Run();
+
+
+
